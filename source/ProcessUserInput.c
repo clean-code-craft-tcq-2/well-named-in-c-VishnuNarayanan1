1de1f386df0a58834e8d993d16c4c0095a64e69a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-08 16:21:14 +0530

    Create ProcessUserInput.c

---
+++ source/ProcessUserInput.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ProcesUserInput.h>
+
+void ProcessUserInput(char userInput)
+{
+  switch(userInput)
+    case '1':
+    PrintColourCodingDatabase();
+    break;
+    case '2':
+    case '3':
+    GetUserInputForAssertion();
+    break;
+    default:
+    break; 
+}
