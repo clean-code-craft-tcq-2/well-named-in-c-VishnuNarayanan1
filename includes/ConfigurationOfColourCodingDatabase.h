57abbed82b08415edfa127779687d82ce8c1f93f
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-09 22:28:44 +0530

    Create ConfigurationOfColourCodingDatabase.h

---
+++ includes/ConfigurationOfColourCodingDatabase.h
@@ -0,0 +1,6 @@
+typedef struct {
+    const char* MajorColourNamesString[];
+    const char* MinorColourNamesString[];
+    enum MajorColor MajorColourNamesEnum;
+    enum MinorColor MinorColourNamesEnum;
+} ColorCodingDatabase;
