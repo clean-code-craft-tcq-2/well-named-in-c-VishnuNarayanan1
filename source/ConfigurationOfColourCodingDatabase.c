e18d2e0ed26a8055c262db6b11a7ba4cd9655e0b
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-09 22:26:24 +0530

    Create ConfigurationOfColourCodingDatabase.c

---
+++ source/ConfigurationOfColourCodingDatabase.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ConfigurationOfColourCodingDatabase.h>
+
+ColourCodingDatabase ColourCodingDatabaseDetails [MaxPossibleColoursInMajorOrMinor] =
+{
+    {"WHITE", "BLUE" , WHITE , BLUE},
+    {"RED", "ORANGE" , RED , ORANGE},
+    {"BLACK", "GREEN" , BLACK , GREEN},
+    {"YELLOW", "BROWN" , YELLOW , BROWN}
+    {"VIOLET" "SLATE" , VIOLET , SLATE},
+}
