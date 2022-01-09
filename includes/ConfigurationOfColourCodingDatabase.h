4ac8430c8dba74fd5c099cb6cd530b6fa09cd0ca
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-09 22:47:27 +0530

    Update ConfigurationOfColourCodingDatabase.h

--- includes/ConfigurationOfColourCodingDatabase.h
+++ includes/ConfigurationOfColourCodingDatabase.h
@@ -1,8 +1,11 @@
 #define MaxPossibleColoursInMajorOrMinor (5u);
 
+enum MajorColour_Enum {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColour_Enum {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
 typedef struct {
     const char* MajorColourNamesString[];
     const char* MinorColourNamesString[];
-    enum MajorColor MajorColourNamesEnum;
-    enum MinorColor MinorColourNamesEnum;
+    enum MajorColour_Enum MajorColourNamesEnum;
+    enum MinorColour_Enum MinorColourNamesEnum;
 } ColorCodingDatabase;
