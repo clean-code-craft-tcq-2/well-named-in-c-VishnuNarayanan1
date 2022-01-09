6f68a0e3ed31021492f5e2e46ac7bad391bb334b
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-09 22:28:02 +0530

    Delete include directory

--- include/ColourCodingDatabase.h
+++
@@ -1,6 +0,0 @@
-typedef struct {
-    const char* MajorColourNamesString[];
-    const char* MinorColourNamesString[];
-    enum MajorColor MajorColourNamesEnum;
-    enum MinorColor MinorColourNamesEnum;
-} ColorCodingDatabase;
