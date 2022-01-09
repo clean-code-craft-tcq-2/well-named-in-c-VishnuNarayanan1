923f233816c266ebcd68082e6d4eb1c6e4677e40
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-09 18:03:41 +0530

    Update GetUserInputForAssertion.c

--- source/GetUserInputForAssertion.c
+++ source/GetUserInputForAssertion.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 #include <GetUserInputForAssertion.h>
+char UserInputMajorColourName[20];
+char UserInputMinorColourName[20];
+int UserInputColourCode;
+
+void GetUserInputForAssertion(void)
+{
+   printf("\n Input Major Colour : ");
+   scanf("%s", UserInputMajorColourName);
+   printf("\n Input Minor Colour : ");
+   scanf("%s", UserInputMinorColourName);
+   printf("\n Input Colour Code : ");
+   scanf("%d", UserInputColourCode);
+}
