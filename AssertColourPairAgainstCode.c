b839ae5553b2db26335e23141cb2808fe777fbb8
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 19:49:59 +0530

    Update AssertColourPairAgainstCode.c

--- AssertColourPairAgainstCode.c
+++ AssertColourPairAgainstCode.c
@@ -9,6 +9,10 @@ void AssertColourPairAgainstCode(UserInputColoursAndCodes userInput)
   userInput.colourCodingOutOfUserInput = userInput.colourCodingOutOfUserInput - 1;
   int majorColourIndex = (userInput.colourCodingOutOfUserInput / MaxPossibleColoursInMajorOrMinor);
   int minorColourIndex = (userInput.colourCodingOutOfUserInput % MaxPossibleColoursInMajorOrMinor);  
+  printf("Major colour Index %d \n" , majorColourIndex);
+  printf("Minor colour Index %d \n" , minorColourIndex);
+  printf("Colour Index %d \n" , userInput.colourCodingOutOfUserInput);
+  
   assert(MajorColourNamesString[majorColourIndex] == MajorColourNamesString[userInput.majorColourIndexOutOfUserInput]);
   assert(MajorColourNamesString[minorColourIndex] == MinorColourNamesString[userInput.minorColourIndexOutOfUserInput]);
 }
