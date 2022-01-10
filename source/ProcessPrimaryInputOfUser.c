5ef24edcd590d62f96e0319283078f8f7f5ec50f
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 18:07:07 +0530

    Update and rename ProcessPrimaryInputOfUser.c to source/ProcessPrimaryInputOfUser.c

--- source/ProcessPrimaryInputOfUser.c
+++ source/ProcessPrimaryInputOfUser.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include "ProcessPrimaryInputOfUser.h"
-#include "PrintColourCodingDatabase.h"
-#include "ValidatePrimaryUserInputForAssertion.h"
+#include "includes/ProcessPrimaryInputOfUser.h"
+#include "includes/PrintColourCodingDatabase.h"
+#include "includes/ValidatePrimaryUserInputForAssertion.h"
 
 void ProcessPrimaryInputOfUser(int userInput)
 {
