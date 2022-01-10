da846b103a9e2ea3d33594b325842ce1f04d43b4
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 13:31:45 +0530

    Delete GetColourPairFromCode.h

--- GetColourPairFromCode.h
+++
@@ -1,6 +0,0 @@
-typedef struct {
-                    char majorColour[];
-                    char minorColour[];
-               } ColourPairFromCode;
-
-ColourPairFromCode GetColourPairFromCode(int colourCode);
