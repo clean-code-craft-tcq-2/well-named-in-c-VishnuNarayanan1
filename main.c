9d5807dc64ae2b6a54a6b07e22bf41e38f897d2c
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 15:47:30 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main()
 {
     int UserInputToProcess;
+    char c;
     printf("------ Data base of colour coding ------\n\n");
     do{
             printf("Enter '1' to print the colour pairs database against the designated code\n\n");
@@ -11,7 +12,8 @@ int main()
             printf("Enter '3' to check whether the colours are matched rightly against the code given\n\n");
             printf("Enter '0' to Exit\n\n");
             printf ("\nIts time for you to enter your input : \n");
-            scanf(" %d",&UserInputToProcess);
+            //scanf(" %d",&UserInputToProcess);
+            c = getchar();
             ProcessPrimaryInputOfUser(UserInputToProcess);
       } while((UserInputToProcess == 1) || (UserInputToProcess == 2) || (UserInputToProcess == 3));
 }
