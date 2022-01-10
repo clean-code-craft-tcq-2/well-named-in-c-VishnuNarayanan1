78af8cb60b76333ffa03c36e9b8de35bc9a74cc8
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 11:42:01 +0530

    Update ConfigurationOfColourCodingDatabase.c

--- source/ConfigurationOfColourCodingDatabase.c
+++ source/ConfigurationOfColourCodingDatabase.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <ConfigurationOfColourCodingDatabase.h>
 
+const char* MajorColorNamesString[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNamesString[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
 ColourCodingDatabase ColourCodingDatabaseDetails_struct [MaxPossibleColoursInMajorOrMinor] =
 {
-    {"WHITE", "BLUE" , WHITE , BLUE},
-    {"RED", "ORANGE" , RED , ORANGE},
-    {"BLACK", "GREEN" , BLACK , GREEN},
-    {"YELLOW", "BROWN" , YELLOW , BROWN}
-    {"VIOLET" "SLATE" , VIOLET , SLATE},
+    {WHITE , BLUE},
+    {RED , ORANGE},
+    {BLACK , GREEN},
+    {YELLOW , BROWN}
+    {VIOLET , SLATE},
 }
