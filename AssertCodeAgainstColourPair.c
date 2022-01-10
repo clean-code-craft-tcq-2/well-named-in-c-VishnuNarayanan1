03735c9079bced332f7b75be17b9506225075848
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 13:37:35 +0530

    Update AssertCodeAgainstColourPair.c

--- AssertCodeAgainstColourPair.c
+++ AssertCodeAgainstColourPair.c
@@ -6,6 +6,6 @@
 
 void AssertCodeAgainstColourPair(UserInputColoursAndCodes userInput)
 {
-    int colourCodeAgainstColourPair = (userInput.majorColourIndexOutOfUserInput * MaxPossibleColoursInMajorOrMinor) + userInput.minorColourIndexOutOfUserInput + 1
+    int colourCodeAgainstColourPair = ((userInput.majorColourIndexOutOfUserInput * MaxPossibleColoursInMajorOrMinor) + (userInput.minorColourIndexOutOfUserInput) + (1));
     assert(colourCodeAgainstColourPair == userInput.colourCodingOutOfUserInput);
 }
