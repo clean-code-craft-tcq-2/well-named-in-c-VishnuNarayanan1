b3156e0d0f6c555f4c84e1a2444ef0d2c76c65be
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 19:36:52 +0530

    Update AssertWithStandardInputs.c

--- AssertWithStandardInputs.c
+++ AssertWithStandardInputs.c
@@ -3,6 +3,7 @@
 #include "AssertWithStandardInputs.h"
 #include "GetSecondaryInputFromUserForAssertion.h"
 #include "AssertCodeAgainstColourPair.h"
+#include "AssertColourPairAgainstCode.h"
 
 void AssertWithStandardInputs(void)
 {
@@ -11,4 +12,15 @@ void AssertWithStandardInputs(void)
         userInput.minorColourIndexOutOfUserInput  = BROWN;
         userInput.colourCodingOutOfUserInput  = 4;
         AssertCodeAgainstColourPair(userInput);
+        userInput.minorColourIndexOutOfUserInput  = WHITE;
+        userInput.colourCodingOutOfUserInput  = 5;
+        AssertCodeAgainstColourPair(userInput);
+        userInput.majorColourIndexOutOfUserInput  = BLACK;
+        userInput.minorColourIndexOutOfUserInput  = ORANGE;
+        userInput.colourCodingOutOfUserInput  = 12;
+        AssertColourPairAgainstCode(userInput);
+        userInput.majorColourIndexOutOfUserInput  = VIOLET;
+        userInput.minorColourIndexOutOfUserInput  = SLATE;
+        userInput.colourCodingOutOfUserInput  = 25;
+        AssertColourPairAgainstCode(userInput);
  }
