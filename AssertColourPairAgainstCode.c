4bc349fc528432d094e111a3265d6bafe6f004c1
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 19:53:53 +0530

    Update AssertColourPairAgainstCode.c

--- AssertColourPairAgainstCode.c
+++ AssertColourPairAgainstCode.c
@@ -8,13 +8,7 @@ void AssertColourPairAgainstCode(UserInputColoursAndCodes userInput)
 {
   userInput.colourCodingOutOfUserInput = userInput.colourCodingOutOfUserInput - 1;
   int majorColourIndex = (userInput.colourCodingOutOfUserInput / MaxPossibleColoursInMajorOrMinor);
-  int minorColourIndex = (userInput.colourCodingOutOfUserInput % MaxPossibleColoursInMajorOrMinor);  
-  printf("Major colour Index %d \n" , majorColourIndex);
-  printf("Minor colour Index %d \n" , minorColourIndex);
-  printf("Colour Index %d \n" , userInput.colourCodingOutOfUserInput);
-    printf("Major colour Index %d \n" , userInput.majorColourIndexOutOfUserInput);
-  printf("Minor colour Index %d \n" , userInput.minorColourIndexOutOfUserInput);
-  
-  //assert(MajorColourNamesString[majorColourIndex] == MajorColourNamesString[userInput.majorColourIndexOutOfUserInput]);
-  //assert(MajorColourNamesString[minorColourIndex] == MinorColourNamesString[userInput.minorColourIndexOutOfUserInput]);
+  int minorColourIndex = (userInput.colourCodingOutOfUserInput % MaxPossibleColoursInMajorOrMinor);    
+  assert(MajorColourNamesString[majorColourIndex] == MajorColourNamesString[userInput.majorColourIndexOutOfUserInput]);
+  assert(MinorColourNamesString[minorColourIndex] == MinorColourNamesString[userInput.minorColourIndexOutOfUserInput]);
 }
