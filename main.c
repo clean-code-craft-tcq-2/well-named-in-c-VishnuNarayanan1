d880b79f774dcc28f8a1f6894574afda43131d73
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 20:05:13 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,21 +7,22 @@
 int main()
 {
     int UserInputToProcess;
-    printf("------ Data base of colour coding ------\n\n");
+    printf("------ Welcome to the data base of colour coding ------\n\n");
     if(OperationTypeConfigured_Enum == PerformStandardOperations)
     {
+        printf("------ Execution based on standard inputs as per configuration ------\n");
         PrintColourCodingDatabase();
         AssertWithStandardInputs();
     }
     else
     {
+        printf("------ Execution based on user inputs as per configuration ------\n");
         do{
                 printf("Enter '1' to print the colour pairs database against the designated code\n\n");
                 printf("Enter '2' to check whether the code is matched rightly against the pair of colours given\n\n");
                 printf("Enter '3' to check whether the colours are matched rightly against the code given\n\n");
                 printf("Enter '0' to Exit\n\n");
                 printf ("\nIts time for you to enter your input : \n");
-                scanf(" %d",&UserInputToProcess);
                 scanf(" %d",&UserInputToProcess);
                 ProcessPrimaryInputOfUser(UserInputToProcess);
           } while((UserInputToProcess == 1) || (UserInputToProcess == 2) || (UserInputToProcess == 3));
