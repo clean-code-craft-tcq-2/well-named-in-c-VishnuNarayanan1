818fc2232a14d194e61c3b16dca6531d63e59ebd
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 13:19:44 +0530

    Update AssertCodeAgainstColourPair.c

--- AssertCodeAgainstColourPair.c
+++ AssertCodeAgainstColourPair.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <assert.h>
 #include <GetSecondaryInputFromUserForAssertion.h>
 #include <ConfigurationOfColourCodingDatabase.h>
 #include <AssertCodeAgainstColourPair.h>
 
 void AssertCodeAgainstColourPair(UserInputColoursAndCodes userInput)
 {
-  int colourCodeAgainstColourPair = (userInput.majorColourIndexOutOfUserInput * MaxPossibleColoursInMajorOrMinor) + userInput.minorColourIndexOutOfUserInput + 1
-  assert(colourCodeAgainstColourPair == userInput.colourCodingOutOfUserInput);
+    int colourCodeAgainstColourPair = (userInput.majorColourIndexOutOfUserInput * MaxPossibleColoursInMajorOrMinor) + userInput.minorColourIndexOutOfUserInput + 1
+    assert(colourCodeAgainstColourPair == userInput.colourCodingOutOfUserInput);
 }
