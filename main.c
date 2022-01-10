67a3bf791102f49e7b314efd485ba93a3dcc739f
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 18:35:05 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include "ConfigurationOfColourCodingDatabase.h"
 #include "ProcessPrimaryInputOfUser.h"
 
 int main()
 {
     int UserInputToProcess;
     printf("------ Data base of colour coding ------\n\n");
-    do{
-            printf("Enter '1' to print the colour pairs database against the designated code\n\n");
-            printf("Enter '2' to check whether the code is matched rightly against the pair of colours given\n\n");
-            printf("Enter '3' to check whether the colours are matched rightly against the code given\n\n");
-            printf("Enter '0' to Exit\n\n");
-            printf ("\nIts time for you to enter your input : \n");
-            scanf(" %d",&UserInputToProcess);
-            ProcessPrimaryInputOfUser(UserInputToProcess);
-      } while((UserInputToProcess == 1) || (UserInputToProcess == 2) || (UserInputToProcess == 3));
+    if(OperationTypeConfigured_Enum == PerformStandardOperations)
+    {
+        
+    }
+    else
+    {
+        do{
+                printf("Enter '1' to print the colour pairs database against the designated code\n\n");
+                printf("Enter '2' to check whether the code is matched rightly against the pair of colours given\n\n");
+                printf("Enter '3' to check whether the colours are matched rightly against the code given\n\n");
+                printf("Enter '0' to Exit\n\n");
+                printf ("\nIts time for you to enter your input : \n");
+                scanf(" %d",&UserInputToProcess);
+                ProcessPrimaryInputOfUser(UserInputToProcess);
+          } while((UserInputToProcess == 1) || (UserInputToProcess == 2) || (UserInputToProcess == 3));
+    }
 }
