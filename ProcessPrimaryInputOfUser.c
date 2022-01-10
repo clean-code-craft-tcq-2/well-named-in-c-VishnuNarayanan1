a9d76944b45f1a1d550ebef4da11d719e6f96705
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-10 13:48:40 +0530

    Update ProcessPrimaryInputOfUser.c

--- ProcessPrimaryInputOfUser.c
+++ ProcessPrimaryInputOfUser.c
@@ -6,14 +6,16 @@
 void ProcessPrimaryInputOfUser(char userInput)
 {
   switch(userInput)
-    case '1':
-        PrintColourCodingDatabase();
-      break;
-    case '2':
-    case '3':
-        ValidatePrimaryUserInputForAssertion(userInput);
-      break;
-    default:
-        printf("\n Valid user input is required to proceed ... Terminating ...\n");
-      break; 
+  {
+      case '1':
+          PrintColourCodingDatabase();
+        break;
+      case '2':
+      case '3':
+          ValidatePrimaryUserInputForAssertion(userInput);
+        break;
+      default:
+          printf("\n Valid user input is required to proceed ... Terminating ...\n");
+        break; 
+  }
 }
