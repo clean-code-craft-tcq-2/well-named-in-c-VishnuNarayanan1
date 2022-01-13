ac317ff1f5ed0ffe2b1e7e0b0199ff1af86c851a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 12:36:01 +0530

    Update ConfigurationOfColourCodingDatabase.h

--- ConfigurationOfColourCodingDatabase.h
+++ ConfigurationOfColourCodingDatabase.h
@@ -3,6 +3,7 @@
 
 enum MajorColour_Enum {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColour_Enum {BLUE, ORANGE, GREEN, BROWN, SLATE};
+enum UserInputOptions {PRINT_COLOUR_CODING_DATABASE, GET_CODE_AGAINST_COLOURPAIR, GET_COLOURPAIR_AGAINST_CODE};
 
 enum OperationType {PerformStandardOperations, PerformOperationsBasedOnUserInput};
 
