a871fdf9ad0ee49f5e9be68640a305bdaebe1059
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 12:19:46 +0530

    Update ConfigurationOfColourCodingDatabase.h

--- ConfigurationOfColourCodingDatabase.h
+++ ConfigurationOfColourCodingDatabase.h
@@ -6,11 +6,12 @@ enum MinorColour_Enum {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 enum OperationType {PerformStandardOperations, PerformOperationsBasedOnUserInput};
 
+/*
 typedef struct {
     enum MajorColour_Enum MajorColourNamesEnum;
     enum MinorColour_Enum MinorColourNamesEnum;
 } ColourCodingDatabase;
-
+*/
 extern const char* MajorColourNamesString[];
 extern const char* MinorColourNamesString[];
 extern enum OperationType OperationTypeConfigured_Enum;
