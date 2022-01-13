c23a5057a28d634acc0f01c02cf66d3eb9fc2961
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 18:01:41 +0530

    Update StatusOfPrintColourCodingDatabase.c

--- StatusOfPrintColourCodingDatabase.c
+++ StatusOfPrintColourCodingDatabase.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 include "StatusOfPrintColourCodingDatabase.h"
 
-static int Status_Print_ColourCode_DataBase = 0;
+static int StatusPrintColourCodingDatabase = 0;
 
 void SuccessStatusOfPrintColourCodingDatabase(void)
 {
-  Status_Print_ColourCode_DataBase = 0x5A;
+  StatusPrintColourCodingDatabase = 0x5A;
 }
 
 int ReturnStatusOfPrintColourCodingDatabase(void)
 {
-  return Status_Print_ColourCode_DataBase;
+  return StatusPrintColourCodingDatabase;
 }
 
 void ResetStatusOfPrintColourCodingDatabase(void)
 {
-  Status_Print_ColourCode_DataBase = 0;
+  StatusPrintColourCodingDatabase = 0;
 }
