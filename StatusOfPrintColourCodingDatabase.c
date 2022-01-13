63eb9bf6f8624bd69c8941c0c7799f646c2f7b7f
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 17:49:22 +0530

    Create StatusOfPrintColourCodingDatabase.c

---
+++ StatusOfPrintColourCodingDatabase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+include <StatusOfPrintColourCodingDatabase.h>
+
+int Status_Print_ColourCode_DataBase = 0;
+
+void SuccessStatusOfPrintColourCodingDatabase(void)
+{
+  Status_Print_ColourCode_DataBase = 0x5A;
+}
+
+int ReturnStatusOfPrintColourCodingDatabase(void)
+{
+  return Status_Print_ColourCode_DataBase;
+}
+
+void ResetStatusOfPrintColourCodingDatabase(void)
+{
+  Status_Print_ColourCode_DataBase = 0;
+}
