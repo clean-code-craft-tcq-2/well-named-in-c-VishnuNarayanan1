13e1b32e3bcad86ecd19c3b4c446ae9340f5dc61
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 11:34:17 +0530

    Update AssertWithUserInputsCodeAgainstColourPair.c

--- AssertWithUserInputsCodeAgainstColourPair.c
+++ AssertWithUserInputsCodeAgainstColourPair.c
@@ -6,6 +6,6 @@
 
 void AssertWithUserInputsCodeAgainstColourPair(UserInputColoursAndCodes userInput)
 {
-    int colourCodeAgainstColourPair = ((userInput.majorColourIndexOutOfUserInput * MaxPossibleColoursInMajorOrMinor) + (userInput.minorColourIndexOutOfUserInput) + (1));
+    int colourCodeAgainstColourPair = ((userInput.majorColourIndexOutOfUserInput * MaxPossibleMajorColours) + (userInput.minorColourIndexOutOfUserInput) + (1));
     assert(colourCodeAgainstColourPair == userInput.colourCodingOutOfUserInput);
 }
