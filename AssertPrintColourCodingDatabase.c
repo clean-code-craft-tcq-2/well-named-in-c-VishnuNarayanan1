b60218a23f2b0b0529fbcb51e97274d40657d7fe
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 18:58:04 +0530

    Update AssertPrintColourCodingDatabase.c

--- AssertPrintColourCodingDatabase.c
+++ AssertPrintColourCodingDatabase.c
@@ -7,8 +7,7 @@ void AssertPrintColourCodingDatabase(void)
 {
   int statusOfPrintColourCode = 0;
   statusOfPrintColourCode = ReturnStatusOfPrintColourCodingDatabase();
-  printf("\n Assert %d", statusOfPrintColourCode);
-  //assert(statusOfPrintColourCode == 0x54);
+  assert(statusOfPrintColourCode == 84);
   ResetStatusOfPrintColourCodingDatabase();
 }
 
