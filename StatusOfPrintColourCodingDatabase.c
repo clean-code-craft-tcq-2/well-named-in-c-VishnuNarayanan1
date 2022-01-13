7353e626bab2ebec7c127db684bd053af31d4a80
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 18:27:14 +0530

    Update StatusOfPrintColourCodingDatabase.c

--- StatusOfPrintColourCodingDatabase.c
+++ StatusOfPrintColourCodingDatabase.c
@@ -5,21 +5,21 @@ int StatusPrintColourCodingDatabase = 0;
 
 void SuccessStatusOfPrintColourCodingDatabase(void)
 {
-  printf("\n Success1 %d", &StatusPrintColourCodingDatabase);
-  StatusPrintColourCodingDatabase = 1;
-  printf("\n Success2 %d", &StatusPrintColourCodingDatabase);
+  printf("\n Success1 %d", StatusPrintColourCodingDatabase);
+  StatusPrintColourCodingDatabase = 0x54;
+  printf("\n Success2 %d", StatusPrintColourCodingDatabase);
 }
 
 int ReturnStatusOfPrintColourCodingDatabase(void)
 {
-  printf("\n Return1 %d", &StatusPrintColourCodingDatabase);
+  printf("\n Return1 %d", StatusPrintColourCodingDatabase);
   return StatusPrintColourCodingDatabase;
-  printf("\n Return2 %d", &StatusPrintColourCodingDatabase);
+  printf("\n Return2 %d", StatusPrintColourCodingDatabase);
 }
 
 void ResetStatusOfPrintColourCodingDatabase(void)
 {
-  printf("\n Reset1 %d", &StatusPrintColourCodingDatabase);
+  printf("\n Reset1 %d", StatusPrintColourCodingDatabase);
   StatusPrintColourCodingDatabase = 0;
-  printf("\n Reset2 %d", &StatusPrintColourCodingDatabase);
+  printf("\n Reset2 %d", StatusPrintColourCodingDatabase);
 }
