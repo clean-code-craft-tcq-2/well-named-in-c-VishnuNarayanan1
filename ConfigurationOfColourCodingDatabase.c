44e35c4d1a3d9c804b661949c7f3057ec35e0702
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 12:22:55 +0530

    Update ConfigurationOfColourCodingDatabase.c

--- ConfigurationOfColourCodingDatabase.c
+++ ConfigurationOfColourCodingDatabase.c
@@ -4,14 +4,4 @@
 const char* MajorColourNamesString[] = {"WHITE", "RED", "BLACK", "YELLOW", "VIOLET"};
 const char* MinorColourNamesString[] = {"BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"};
 
-/*
-ColourCodingDatabase ColourCodingDatabaseDetails_struct [MaxPossibleColoursInMajorOrMinor] =
-{
-    {WHITE , BLUE},
-    {RED , ORANGE},
-    {BLACK , GREEN},
-    {YELLOW , BROWN},
-    {VIOLET , SLATE},
-};
-*/
 enum OperationType OperationTypeConfigured_Enum = PerformStandardOperations;
