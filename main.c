b88f2674e853670c408bce64853183f9ab40d196
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-01-13 16:12:35 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "ConfigurationOfColourCodingDatabase.h"
 #include "ProcessPrimaryInputOfUser.h"
 #include "PrintColourCodingDatabase.h"
@@ -6,7 +7,7 @@
 
 int main()
 {
-    int UserInputToProcess;
+    char UserInputToProcess[30];
     printf("------ Welcome to the data base of colour coding ------\n\n");
     if(OperationTypeConfigured_Enum == PerformStandardOperations)
     {
@@ -17,14 +18,12 @@ int main()
     else
     {
         printf("------ Execution based on user inputs as per configuration ------\n");
-        do{
-                printf("Enter '1' to print the colour pairs database against the designated code\n\n");
-                printf("Enter '2' to check whether the code is matched rightly against the pair of colours given\n\n");
-                printf("Enter '3' to check whether the colours are matched rightly against the code given\n\n");
-                printf("Enter '0' to Exit\n\n");
-                printf ("\nIts time for you to enter your input : \n");
-                scanf(" %d",&UserInputToProcess);
-                ProcessPrimaryInputOfUser(UserInputToProcess);
-          } while((UserInputToProcess == 1) || (UserInputToProcess == 2) || (UserInputToProcess == 3));
+        printf("Enter PRINT_COLOUR_CODING_DATABASE to print the colour pairs database against the designated code\n\n");
+        printf("Enter GET_CODE_AGAINST_COLOURPAIR to check whether the code is matched rightly against the pair of colours given\n\n");
+        printf("Enter GET_COLOURPAIR_AGAINST_CODE to check whether the colours are matched rightly against the code given\n\n");
+        printf("Enter EXIT to terminate\n\n");
+        printf ("\nIts time for you to enter your input : \n");
+        scanf("%s",UserInputToProcess);
+        ProcessPrimaryInputOfUser(UserInputToProcess);
     }
 }
